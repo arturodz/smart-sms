8e36efef669dbd940efff8904aabbbd947fb0b7f
Arturo Diaz <arturo.diaz@wavesoluciones.com>
2014-11-01 04:43:26 -0600

    x

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,8 +23,9 @@ class MessagesController < ApplicationController
   end
 
   def incoming_mail
-    puts params["To"]
-    
+    puts params
+    puts 'hellp'
+
     render json: nil, status: 200
   end
 end
